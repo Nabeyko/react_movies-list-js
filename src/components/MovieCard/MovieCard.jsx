69e52fa59c14d0389888092748861dcc7bcf01e4
Nazar Beyko <nazarbeyko12@gmail.com>
2025-12-15 13:30:01 +0200

    changed MovieList

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,10 +1,10 @@
 import './MovieCard.scss';
 
 export const MovieCard = ({ movie }) => {
-  const { title, imgUrl, description, imdbUrl, imdbId } = movie;
+  const { title, imgUrl, description, imdbUrl } = movie;
 
   return (
-    <div className="card" data-cy="Movie" key={imdbId}>
+    <div className="card" data-cy="Movie">
       <div className="card-image">
         <figure className="image is-4by3">
           <img data-cy="MovieImage" src={imgUrl} alt={title} />
